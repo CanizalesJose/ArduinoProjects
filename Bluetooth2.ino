0d9f69bc93b882e1d8162efc7cbee8b67ddac85d
CanizalesJose <jose1605m@gmail.com>
2024-05-16 20:35:19 -0700

    Agregado funcionamiento básico y explicaciones

--- Bluetooth2.ino
+++ Bluetooth2.ino
@@ -1,7 +1,24 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BT(0, 1);
+SoftwareSerial BT(10, 11);
 
 void setup(){
   Serial.Begin(9600);
+  BT.begin(38400);
+}
 
+void loop(){
+  //Devuelve verdadero cuando hay datos disponibles desde el módulo
+  //Unicamente se entra en la sentencia cuando hay datos disponibles desde el módulo
+  if (BT.available()){
+    // BT.read() lee el dato que se encuentra en el modulo
+    Serial.write(BT.read());
+  }
+
+  //Proceso inverso al anterior
+  //Detecta cuando hay información en el monitor serial y lo puedes denviar al módulo
+  if (Serial.available()){
+    //Serial.read() es el dato dentro del monitor serial
+    BT.write(Serial.read());
+
+  }
 }
